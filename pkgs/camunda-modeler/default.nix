b32918f7c9aa9790a14f7e02fedcdabc8ee3bdf8
Asko Soukka <asko.soukka@iki.fi>
2023-03-08 13:40:00 +0200

    camunda-modeler: 5.8.0; zeebe-play: 1.6.0

--- pkgs/camunda-modeler/default.nix
+++ pkgs/camunda-modeler/default.nix
@@ -2,7 +2,7 @@
 
 let
 
-  modelerVersion = "5.7.0";
+  modelerVersion = "5.8.0";
 
   mkElectron = import "${pkgs.path}/pkgs/development/tools/electron/generic.nix" { inherit stdenv libXScrnSaver makeWrapper fetchurl wrapGAppsHook glib gtk3 unzip atomEnv libuuid at-spi2-atk at-spi2-core libdrm mesa libxkbcommon libxshmfence lib libappindicator-gtk3 libglvnd; };
   electron = mkElectron "20.2.0" {
@@ -10,8 +10,8 @@ let
   };
 
   bpmn-js-token-simulation-plugin = fetchurl {
-    url = "https://github.com/bpmn-io/bpmn-js-token-simulation-plugin/archive/b433c1454e0edf415372b58fe897569b75cf2c40.tar.gz";
-    sha256 = "sha256-5D1wElJrwZeKoiWhVRv2UCaLTs5jebZCTaKrOr9ZgF4=";
+    url = "https://github.com/bpmn-io/bpmn-js-token-simulation-plugin/archive/85aa974ae5877dfa4bca585c258132a429566dca.tar.gz";
+    sha256 = "sha256-/g9YuE48XbVy/zdFTWgV568Z7Yxp9hNKh9BsI16meyc=";
   };
 
   camunda-modeler-robot-plugin = fetchurl {
@@ -25,7 +25,7 @@ let
     version = modelerVersion;
     src = fetchurl {
       url = "https://github.com/camunda/camunda-modeler/releases/download/v${version}/camunda-modeler-${version}-linux-x64.tar.gz";
-      sha256 = "sha256-eFCvq9ecQ4WFnaibKWvhQe/VmOo62LipJUIXna1+aVY=";
+      sha256 = "sha256-v1K3CIE+gYnhn2af5Rzx8R3/M/42xTIjHXQ69pgnsZI=";
     };
     nativeBuildInputs = [ nodePackages.asar autoPatchelfHook gcc-unwrapped ];
     installPhase = ''
--- pkgs/zeebe-play/default.nix
+++ pkgs/zeebe-play/default.nix
@@ -6,30 +6,25 @@ let
     sha256 = "cb26937b3b9f2382647e5ce964e4fd8abf274d59f04164aba12a80ad81c726ae";
   };
 
-  formViewer = fetchurl {
-    url = "https://unpkg.com/@bpmn-io/form-js@0.10.1/dist/form-viewer.umd.js";
-    sha256 = "2d98d53a2c03e58706d17eb071ee87a296d20c9f3585e32eb58a9da6d215dacd";
-  };
-
 in
 
 stdenv.mkDerivation rec {
   pname = "zeebe-play";
-  version = "1.2.0-alpha1";
+  version = "1.6.0";
   name = "${pname}-${version}";
   src = fetchurl {
     url = "https://github.com/camunda-community-hub/${pname}/releases/download/${version}/${name}.zip";
-    sha256 = "f1bd9ab0ca95c4121450ce2071a542f0c61a82acf514f78d5bc038737287d534";
+    sha256 = "sha256-l3P2W7ikz8zsnKYkae9mpcN0CqJ2/u7ppbfbcFM9UbQ=";
   };
   nativeBuildInputs = [ unzip ];
   buildInputs = [ makeWrapper ];
   propagatedBuildInputs = [ jdk ];
   installPhase = ''
 mkdir -p $out/usr/share/lib $out/bin
-cp nexus-staging/deferred/org/camunda/community/${pname}/${version}/${name}.jar $out/usr/share/lib
+cp nexus-staging/staging/*/org/camunda/community/${pname}/${version}/${name}.jar $out/usr/share/lib
 
-jar xf nexus-staging/deferred/org/camunda/community/${pname}/${version}/${name}.jar BOOT-INF/classes/templates/fragments/footer.html
-jar xf nexus-staging/deferred/org/camunda/community/${pname}/${version}/${name}.jar BOOT-INF/classes/public/js/view-bpmn.js
+jar xf nexus-staging/staging/*/org/camunda/community/${pname}/${version}/${name}.jar BOOT-INF/classes/templates/fragments/footer.html
+jar xf nexus-staging/staging/*/org/camunda/community/${pname}/${version}/${name}.jar BOOT-INF/classes/public/js/view-bpmn.js
 
 substituteInPlace BOOT-INF/classes/templates/fragments/footer.html \
     --replace \
@@ -39,12 +34,10 @@ substituteInPlace BOOT-INF/classes/public/js/view-bpmn.js \
     --replace \
     "height: '100%'" "height: '100%', additionalModules: [ RobotTaskModule ]"
 cp ${robotTask} BOOT-INF/classes/public/js/robot-task.js
-cp ${formViewer} BOOT-INF/classes/public/js/form-viewer.umd.js
 
 jar uf $out/usr/share/lib/${name}.jar BOOT-INF/classes/templates/fragments/footer.html
 jar uf $out/usr/share/lib/${name}.jar BOOT-INF/classes/public/js/robot-task.js
 jar uf $out/usr/share/lib/${name}.jar BOOT-INF/classes/public/js/view-bpmn.js
-jar uf $out/usr/share/lib/${name}.jar BOOT-INF/classes/public/js/form-viewer.umd.js
 
 cat << EOF >> $out/bin/.zeebe-play
 #!/usr/bin/env sh
